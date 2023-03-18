a75c0c39611bb3f3253503507f4493c697de06a8
tesa <tej.sanghavi@nordicsemi.no>
2023-03-18 14:23:04 +0100

    Create an enum for ALU logical operations and use it in the ALU.

--- single-cycle/rtl/alu.sv
+++ single-cycle/rtl/alu.sv
@@ -1,20 +1,24 @@
 `default_nettype none
 
+import pa_riscv::*;
+
 module alu
   ( input  var logic [31:0] i_a
   , input  var logic [31:0] i_b
 
-  , input  var logic [1:0]  i_aluControl
+  , input  var logic [3:0]  i_aluLogicalOperation
 
   , output var logic [31:0] o_result
   );
 
+  // TODO: Add more logical operations.
   always_comb
-    case (i_aluControl)
-      2'b00:   o_result = i_a + i_b;         // Add
-      2'b01:   o_result = i_a + (~i_b+1'b1); // Subtract
-      2'b10:   o_result = i_a & i_b;         // And
-      2'b11:   o_result = i_a | i_b;         // Or
+    case (i_aluLogicalOperation)
+      ADD:     o_result = i_a + i_b;
+      SUB:     o_result = i_a + (~i_b+1'b1);
+      AND:     o_result = i_a & i_b;
+      OR:      o_result = i_a | i_b;
+      XOR:     o_result = i_a ^ i_b;
       default: o_result = 32'bx;
     endcase
 
--- single-cycle/rtl/pa_riscv.sv
+++ single-cycle/rtl/pa_riscv.sv
@@ -11,6 +11,14 @@ package pa_riscv;
   , R = 7'b0110011
   } ty_INSTRUCTION_TYPE;
 
+  typedef enum logic [3:0]
+  { ADD = 4'b0000
+  , SUB = 4'b1000
+  , AND = 4'b0111
+  , OR  = 4'b0110
+  , XOR = 4'b0100
+  } ty_ALU_OP;
+
 endpackage
 
 `resetall
