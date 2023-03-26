fbd4fa2595f78903088908ede52124d080268e54
tesa <tej.sanghavi@nordicsemi.no>
2023-03-26 18:28:17 +0200

    Decode operand to determine if it's a JAL instruction.

--- single-cycle/rtl/pa_riscv.sv
+++ single-cycle/rtl/pa_riscv.sv
@@ -11,6 +11,7 @@ package pa_riscv;
   , R_TYPE_ALU = 7'b0110011
   , B_TYPE     = 7'b1100011
   , I_TYPE_ALU = 7'b0010011
+  , JAL        = 7'b1101111
   } ty_INSTRUCTION_TYPE;
 
   typedef enum logic [3:0]
